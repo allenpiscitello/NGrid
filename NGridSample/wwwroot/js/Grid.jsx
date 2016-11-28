89ae6e78935961ddc7390766c8007f7b1d16f8ef
Allen Piscitello <allen.piscitello@headspring.com>
2016-11-28 17:05:32 -0600

    Adding data.

--- NGridSample/wwwroot/js/Grid.jsx
+++ NGridSample/wwwroot/js/Grid.jsx
@@ -1,12 +1,24 @@
 ﻿var Grid = React.createClass({
 
     getInitialState: function() {
-        return { Columns: [{ Name: "Column1" }, { Name: "Column2" }] };
+        return {
+            Columns: [{ Name: "Column1" }, { Name: "Column2" }],
+            Data: [{ Column1: "Item 1", Column2: "Item 2" }, { Column1: "Item 3", Column2: "Item 4" }]
+        };
     },
     render: function () {
-        var columns = this.state.Columns.map(function (item) {
-            return (<th>{item.Name}</th>)
-        })
+        var columns = this.state.Columns.map(function (item, index) {
+            return (<th key={index}>{item.Name}</th>)
+        });
+
+        var columnData = this.state.Columns;
+
+        var rows = this.state.Data.map(function (item, index) {
+            var cols = columnData.map(function (col, index) {
+                return (<td key={index}>{item[col.Name]}</td>)
+            });
+            return (<tr key={index}>{cols}</tr>)
+        });
 
         return (
           <div className="table-responsive">
@@ -17,7 +29,7 @@
                 </tr>
                 </thead>
                 <tbody>
-                    <tr><td>item1</td><td>item2</td></tr>
+                    {rows}
                 </tbody>
             </table>
           </div>
