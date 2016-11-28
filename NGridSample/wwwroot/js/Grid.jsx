00d4ce0a13ed4efe0ba6feaf114a439af1446954
Allen Piscitello <allen.piscitello@headspring.com>
2016-11-28 16:48:55 -0600

    Adding basic grid with fixed data.

--- NGridSample/wwwroot/js/Grid.jsx
+++ NGridSample/wwwroot/js/Grid.jsx
@@ -1,8 +1,17 @@
 ﻿var Grid = React.createClass({
     render: function() {
         return (
-          <div>
-            Hello, world! I am a Grid Box.
+          <div className="table-responsive">
+            <table className="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                    <th>Column1</th><th>Column2</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr><td>item1</td><td>item2</td></tr>
+                </tbody>
+            </table>
           </div>
       );
     }
